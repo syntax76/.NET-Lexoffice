b37edac7e2c64e79640638d175d77a0ef372d414
Heinrich Alexandra Hermann <a.hermann@hermann-bsd.de>
2020-12-13 19:29:38 +0100

    Generic EventArgs added.

---
+++ NET-Lexoffice/IEventArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace ahbsd.lib
+{
+    /// <summary>
+    /// Interface for generic EventArgs.
+    /// </summary>
+    /// <typeparam name="T">Type of Value.</typeparam>
+    public interface IEventArgs<T>
+    {
+        /// <summary>
+        /// Returns the value.
+        /// </summary>
+        /// <value>The value.</value>
+        T Value { get; }
+    }
+}
